e1db4c3c24f94e6667e27c780a097147371311b5
Hanna Prinz <hanna_prinz@yahoo.de>
2018-01-08 20:47:39 +0100

    Step 2: Hello Pretty World

--- App.js
+++ App.js
@@ -1,14 +1,25 @@
-// App.js
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 
 export default class HelloWorld extends Component {
   render() {
     return (
-      <View>
-        <Text>Hello World!</Text>
+      <View style={styles.prettyView}>
+        <Text style={styles.prettyText}>Hello World!</Text>
       </View>
     );
   }
 }
 
+const styles = StyleSheet.create({
+  prettyText: {
+    fontSize: 30,
+    fontWeight: "900",
+    color: "pink",
+    textAlign: "center"
+  },
+  prettyView: {
+    flex: 1,
+    justifyContent: "center"
+  }
+})
