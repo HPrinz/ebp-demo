c3965faf190011fde8991c0f49288d77981729d0
Hanna Prinz <hanna_prinz@yahoo.de>
2018-01-08 20:48:16 +0100

    Step 3: Hello Native World

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, Picker } from 'react-native';
 
 export default class HelloWorld extends Component {
   render() {
     return (
       <View style={styles.prettyView}>
         <Text style={styles.prettyText}>Hello World!</Text>
+        <Picker selectedValue="react">
+          <Picker.Item label="Hybrid" value="hybrid" />
+          <Picker.Item label="React Native" value="react" />
+          <Picker.Item label="Native" value="native" />
+        </Picker>
       </View>
     );
   }
@@ -13,13 +18,13 @@ export default class HelloWorld extends Component {
 
 const styles = StyleSheet.create({
   prettyText: {
-    fontSize: 30,
-    fontWeight: "900",
-    color: "pink",
+    fontSize: 30, 
+    fontWeight: "900", 
+    color: "pink", 
     textAlign: "center"
   },
   prettyView: {
-    flex: 1,
+    flex: 1, 
     justifyContent: "center"
   }
 })
